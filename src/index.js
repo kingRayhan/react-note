03e517f812584602422e5e8f1ee9e38a15584311
kingrayhan <rayhan095@gmail.com>
2019-08-16 17:40:35 +0600

    design updated

--- src/index.js
+++ src/index.js
@@ -4,13 +4,18 @@ import './index.scss'
 
 const Note = ({ bgColor = '#fff' }) => (
     <div className="note" style={{ backgroundColor: bgColor }}>
-        <h3 className="title">Lorem ipsum dolor sit amet.</h3>
-        <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto dolorem ea fugiat ad maiores distinctio
-            impedit dignissimos possimus blanditiis veritatis eligendi earum dolorum commodi quasi quam a, incidunt
-            illo. Illum.
-        </p>
+        <div>
+            <h3 className="title">Lorem ipsum dolor sit amet.</h3>
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto dolorem ea fugiat ad maiores
+                distinctio impedit dignissimos possimus blanditiis veritatis eligendi earum dolorum commodi quasi quam
+                a, incidunt illo. Illum.
+            </p>
+        </div>
         <p className="time">5 minutes ago</p>
+        <div className="buttons">
+            <button className="delete">&times;</button>
+        </div>
     </div>
 )
 
